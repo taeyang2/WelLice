95175f20d8dc3847ad5569b502688fdb44f7f48b
taeyang <taeyang2.lee@gmail.com>
2020-10-08 03:26:30 +0900

    effect_ingredient.json 형태로 정리하는 소스 추가

--- WelLice_Efficacy.py
+++ WelLice_Efficacy.py
@@ -11,7 +11,7 @@ stuffData = {}
 efficacyDict = {}
 
 url = "https://terms.naver.com/list.nhn?cid=42785&categoryId=42795&so=st3.asc&viewType=&categoryType=&page="
-for pageNum in range(2): #101 페이지
+for pageNum in range(4): #101 페이지
     pageNum = pageNum +1
     pageUrl = url + str(pageNum)
     print(pageUrl)
@@ -93,12 +93,53 @@ print("최종결과!!!!!!!!", efficacyDict)
 print(len(efficacyDict))
 
 
-# 효능 데이터 확인
+# 효능 데이터 확인 & Json
+effect_igredient_list = []
+
 count = 0
 for ekey, evalue in efficacyDict.items():
     if evalue != '결과 없음':
         count = count + 1
         print("데이터확인---"+str(count)+"."+ekey)
+
+        # Json
+        for e5 in evalue:
+            print("e5----",e5)
+            if len(effect_igredient_list) != 0:
+                print('1')
+                in_effect = []
+                for e6 in effect_igredient_list:
+                    print("e6['effect']====",e6['effect'])
+                    in_effect.append(e6['effect'])
+                    print("in_effect---",in_effect)
+                    #print("e6----",e6)
+                if e5 not in in_effect:
+                    print('1-1')
+                    effect_igredient_list.append({'effect': e5, 'ingredient_list': [ekey]})
+                    print("RESULT_effect_igredient_list", effect_igredient_list)
+
+
+                elif e5 in in_effect:
+                    print('1-2')
+                    for e7 in effect_igredient_list:
+                        if e7['effect'] == e5:
+                            e7['ingredient_list'].append(ekey)
+                            print("RESULT_effect_igredient_list", effect_igredient_list)
+
+                    #effect_igredient['ingredient_list'].append(ekey)
+                    #print("effect_igredient['ingredient_list']",effect_igredient['ingredient_list'])
+
+            elif len(effect_igredient_list) == 0 :
+                print('2')
+                effect_igredient = {}
+                effect_igredient['effect'] = e5
+                effect_igredient['ingredient_list'] = [ekey]
+                effect_igredient_list.append(effect_igredient)
+                print("1st_not in_effect_igredient----------", effect_igredient)
+
+print(effect_igredient_list)
+
+
 
 
 
